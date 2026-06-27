09e57f99e9dc9f994e44ab9525f583d2c30f5c9d
YuShiTing031 <125135149+YuShiTing031@users.noreply.github.com>
2026-06-27 19:55:17 +0800

    Update main.py

--- spfbot/main.py
+++ spfbot/main.py
@@ -14,7 +14,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 TARGET_URL = "https://www.spf.com.tw/sinopacSPF/research/list.do?id=1709f20d3ff00000d8e2039e8984ed51"
 IMAGE_DIR = "images" 
-WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
+WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK")
 
 def get_latest_pdf_url():
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
